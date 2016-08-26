4abf48c6b744e4c7e3bd15f2df9feebcf949c6d9
adamshire123 <adamshire123@gmail.com>
2016-08-26 02:43:29 +0000

    create directory

---
+++ js/directory.js
@@ -0,0 +1,58 @@
+var app = angular.module('directory', []).controller('searchCtrl', function($scope) {
+    $scope.staff = [
+    {
+      firstName:'Michael',
+      lastName:'Brewer',
+      title:'Head, Research & Learning',
+      office:'MAIN A204B',
+      department:'Research and Learning (R&L)',
+      phone:'520-307-2771',
+      email:'brewerm@email.arizona.edu',
+      photo:'https://adamshire123.github.io/staff-directory/images/brewerm.jpg'
+    },
+      {
+      firstName:'Cheryl',
+      lastName:'Cuillier',
+      title:'Associate Librarian',
+      office:'MAIN A207',
+      department:'Research and Learning (R&L)',
+      phone:'520-310-9874',
+      email:'ccuillie@email.arizona.edu',
+      photo:'https://adamshire123.github.io/staff-directory/images/cuillierc.jpg'},
+       {
+      firstName:'Jason',
+      lastName:'Dewland',
+      title:'Assistant Librarian',
+      office:'MAIN A207',
+      department:'Research and Learning (R&L)',
+      phone:'520-447-8097',
+      email:'jasondewland@email.arizona.edu',
+      photo:'https://adamshire123.github.io/staff-directory/images/dewlandj.jpg'
+      },
+       {
+      firstName:'Mary',
+      lastName:'Feeney',
+      title:'Associate Librarian',
+      office:'MAIN A213',
+      department:'Research and Learning (R&L)',
+      phone:'520-307-2615',
+      email:'mfeeney@email.arizona.edu',
+      photo:'https://adamshire123.github.io/staff-directory/images/mfeeney.jpg'
+    }
+    ];
+  
+ // Makes the input box query string lowercase
+  	$scope.query = '';
+  	$scope.$watch('query', function() {
+  	    $scope.query = $scope.query.toLowerCase();
+  	});
+  
+  	// Only search certain properties
+    $scope.search = function (row) {
+        return (
+          angular.lowercase(row.firstName).indexOf($scope.query || '') === 0 || 
+          angular.lowercase(row.lastName).indexOf($scope.query || '') === 0);
+    };
+  
+   
+});
